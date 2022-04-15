84968e2e4d956c5f22e51d40aaaa72161bab6267
Cipscis <cipscis+github@gmail.com>
2022-04-15 03:42:50 +0000

    Deploying to gh-pages from @ Cipscis/scroll-appear@e7c94d66a2a26bdeb940b1cd9a1c0a02446e3b99 🚀

--- assets/js/dist/src/ScrollAppearState.d.ts
+++ assets/js/dist/src/ScrollAppearState.d.ts
@@ -1,6 +1,7 @@
 export declare enum ScrollAppearState {
     UNINITIALISED = "uninitialised",
     HIDDEN = "hidden",
+    APPEARING = "appearing",
     VISIBLE = "visible"
 }
 export declare function isScrollAppearState(val: unknown): val is ScrollAppearState;
