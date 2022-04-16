ee187e7e049d73a88483e1cd0c1641eeb582306c
Cipscis <cipscis+github@gmail.com>
2022-04-16 00:13:02 +0000

    Deploying to gh-pages from @ Cipscis/scroll-appear@4b5ac19726ec08d0bd4c56e72c9d133ef255a9be 🚀

--- assets/js/dist/src/domMap.d.ts
+++ assets/js/dist/src/domMap.d.ts
@@ -1,6 +1,5 @@
 export declare const selectors: {
     readonly item: ".js-scroll-appear";
-    readonly newItem: ".js-scroll-appear:not([data-scroll-appear-state])";
     readonly container: ".js-scroll-appear__container";
 };
 export declare const attributes: {
